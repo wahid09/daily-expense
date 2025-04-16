2f0380db2b93e22839cfab5d1625463983fba956
wahid ullah <wahid.ullah.cse@gmail.com>
2025-04-16 23:12:09 +0600

    category list view

--- category/views.py
+++ category/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from .models import Category
 
+
 # Create your views here.
 
 def get_category(request):
-    categories = Category.objects.all()  # get all categories
-    return render(request, 'categories/category_list.html', {'categories': categories})
+    categories = Category.objects.all().order_by('-created_at')  # get all categories
+    print(categories)
+    context = {'categories': categories}
+    return render(request, 'categories/category_list.html', context)
+
+
 def create_category(request):
     pass
